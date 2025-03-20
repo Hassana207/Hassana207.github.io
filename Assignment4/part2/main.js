b4510a0ea70589e407a9b3d249a57423121324c5
Hassana207 <hassan.ayinde@dcmail.ca>
2025-03-20 11:59:52 -0400

    Update main.js
    for loop to iterate through image file and insert an img element inside the thumb bar class that embeds that image in the page along with its alternative text. Added a click event listener to each img inside the thumb-bar so that, when they are clicked, the corresponding image and alternative text are displayed in the displayed img element.

--- Assignment4/part2/main.js
+++ Assignment4/part2/main.js
@@ -6,18 +6,29 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const picArray = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
+const picArray = ["./images/pic1.jpg","./images/pic2.jpg","./images/pic3.jpg","./images/pic4.jpg","./images/pic5.jpg"];
+
+
 
 
 /* Declaring the alternative text for each image file */
 const altImg = {pic1:"eye",pic2:"gallery",pic3:"flowers",pic4:"people",pic5:"butterfly"};
 
 /* Looping through images */
+for (let i = 0; i < picArray.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', picArray[i]);
 
+    const imageKey = `pic${i + 1}`; 
+    newImage.setAttribute('alt', altImg[imageKey]);
+    thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", function () {
+        displayedImage.setAttribute('src', picArray[i]); 
+        displayedImage.setAttribute('alt', altImg[imageKey]); 
+    });
+}
+    
 
 /* Wiring up the Darken/Lighten button */
+
