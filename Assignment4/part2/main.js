04f8756bdb762ab56efdacc44dc2f7745024df8f
Hassana207 <hassan.ayinde@dcmail.ca>
2025-03-20 11:13:56 -0400

    Update main.js
    an array declared to hold image files and  an object to hold alternatives text of each image fille

--- Assignment4/part2/main.js
+++ Assignment4/part2/main.js
@@ -6,9 +6,14 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const picArray = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
+
+
 /* Declaring the alternative text for each image file */
+const altImg = {pic1:"eye",pic2:"gallery",pic3:"flowers",pic4:"people",pic5:"butterfly"};
 
 /* Looping through images */
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
