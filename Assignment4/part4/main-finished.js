6809fbc57eebfac7ea2ec6b3ae08e00a140708c3
Hassana207 <hassan.ayinde@dcmail.ca>
2025-03-28 15:25:06 -0400

    Update main-finished.js
    checkbound() method created to update the location of the evil circle . the first two if statement to update x axes location and the second if statement update y axes location so that it does not go off-screen. collisiondetect() method also created in a way that when ball collide with the evil circle it stop existing.

--- Assignment4/part4/main-finished.js
+++ Assignment4/part4/main-finished.js
@@ -110,6 +110,40 @@ class EvilCircle extends Shape{
       ctx.stroke();
       ctx.lineWidth = 3
     }
+
+    checkBounds() {
+    
+      if (this.x + this.size >= width) {
+        this.x = width - this.size; 
+      }
+    
+      if (this.x - this.size <= 0) {
+        this.x = this.size; 
+      }
+    
+      if (this.y + this.size >= height) {
+        this.y = height - this.size; 
+      }
+    
+      if (this.y - this.size <= 0) {
+        this.y = this.size; 
+      }
+    }
+
+    collisionDetect() {
+      for (const ball of balls) {
+        if (ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+    
+          if (distance < this.size + ball.size) {
+            ball.exists = false;
+          }
+        }
+      }
+    }
+    
   
     
   }
