3e2e4c1b0457ea8e67e10b88bac845264ceb44fd
Hassana207 <hassan.ayinde@dcmail.ca>
2025-04-11 15:20:36 -0400

    main.js
     changed the show comments text to a button.
    
    renamed function to triggerComment, and it should trigger when clicked.
    
    added an event listener to trigger triggerComment when the Enter key or Space key is pressed.

--- Assignment5/main.js
+++ Assignment5/main.js
@@ -5,15 +5,35 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
+
 showHideBtn.onclick = function() {
+  triggerComments();
+};
+
+
+showHideBtn.addEventListener('keydown', function(e) {
+  if (e.key === 'Enter' || e.key === ' ') {
+    e.preventDefault(); 
+    triggerComments();
+  }
+});
+
+function triggerComments() {
+   const isHidden = commentWrapper.style.display === 'none';
+
+
   let showHideText = showHideBtn.textContent;
   if(showHideText === 'Show comments') {
     showHideBtn.textContent = 'Hide comments';
     commentWrapper.style.display = 'block';
+  
+    
   } else {
     showHideBtn.textContent = 'Show comments';
     commentWrapper.style.display = 'none';
+  
   }
+  
 };
 
 // functionality for adding a new comment via the comments form
